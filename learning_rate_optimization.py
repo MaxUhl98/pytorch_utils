0c6f92a2769d5a3ed31752e62107f821d8b0b613
MaxUhl98 <110908917+MaxUhl98@users.noreply.github.com>
2024-04-03 10:40:26 +0200

    Update learning_rate_optimization.py
    Improved logging, made lr step finder work properly

--- learning_rate_optimization.py
+++ learning_rate_optimization.py
@@ -4,7 +4,6 @@ Actually there is a small difference in my implementation to Chris description: 
 Also I want to say a HUGE thank you to Chris Deotte for all his contributions to HMS brain Activity Classification, which where all very informative and helped me grow as a Data Scientist"""
 import os
 import uuid
-
 import torch
 from torch import nn
 from typing import Optional, Union, Dict, Any, Callable, Iterable, Tuple, List
@@ -16,6 +15,35 @@ from _config import Config
 import pandas as pd
 from SelfNet import SelfNet
 from data_generator import GenericData, prepare_train
+
+def get_logger(name:str, base_filepath:str='logs/model_experiments') -> logging.Logger:
+    """Creates a logging.Logger object that writes a logfile named name.log into the folder at base_filepath
+    (throws an error if the folder does not exist)
+
+    :param name: Name of the logger and the logging file
+    :param base_filepath: Path to the folder in which the logging file will get saved
+    :return: Logger object
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler(f'{base_filepath}/{name}.log', encoding='utf-8')
+    fh.setLevel(logging.DEBUG)
+
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    ch.setFormatter(formatter)
+    fh.setFormatter(formatter)
+
+    # add the handlers to logger
+    logger.addHandler(ch)
+    logger.addHandler(fh)
+    return logger
 
 
 def reset_model(model: nn.Module, reset_weight_path: str) -> None:
@@ -31,28 +59,36 @@ def reset_model(model: nn.Module, reset_weight_path: str) -> None:
 def find_best_start_lr(model: nn.Module, optim: Callable, train_loader: DataLoader, test_loader: DataLoader,
                        loss_fn: nn.Module, max_epochs: int = 15, tested_lr_list=None, patience: int = 3,
                        device: Union[str, torch.device] = 'cuda') -> Tuple[float, List[float]]:
-    """
+    """Tests all initial learning rates out of tested_lr_list
+    (default=[1e-6, 5e-6, 1e-5, 5e-5, 1e-4, 5e-4, 1e-3, 5e-3, 1e-2])
+    Returns the learning rate that had the lowest validation loss when training with the data inside the train_loader
+    and validating using the data in val_loader. Trains for max_epoch steps, cancels the training if the validation
+    loss did not increase in the last patience epochs.
 
-    :param model:
-    :param optim:
-    :param train_loader:
-    :param test_loader:
-    :param loss_fn:
-    :param max_epochs:
-    :param tested_lr_list:
-    :param patience:
-    :param device:
-    :return:
+
+    :param model: Model for which the best initial learning rate shall be found
+    :param optim: Optimizer to use for training
+    :param train_loader: DataLoader containing training data
+    :param test_loader: DataLoader containing the validation data
+    :param loss_fn: Function used to calculate the loss
+    :param max_epochs: Maximum number of epochs to train for one learning rate trial
+    :param tested_lr_list: List containing the numeric values of the learning rates that shall be tested
+    :param patience: A trial will get canceled if the validation loss did not improve in the last patience steps
+    :param device: Pytorch Device to train the model on
+    :return: Best performing learning rate from the trials and
+     list containing the validation loss of each step from the trial with the best learning rate
     """
     if tested_lr_list is None:
-        tested_lr_list = [1e-6,5e-6,1e-5, 5e-5, 1e-4, 5e-4, 1e-3, 5e-3, 1e-2]
+        tested_lr_list = [1e-6, 5e-6, 1e-5, 5e-5, 1e-4, 5e-4, 1e-3, 5e-3, 1e-2]
     run_id = uuid.uuid1()
     reset_weight_path = f'{run_id}.pth'
     torch.save(model.state_dict(), reset_weight_path)
     logger = get_logger(f'LR_TEST_{run_id}')
+    logger.log(f'Starting lr optimization run with ID {run_id}')
     lr_data = {}
     best_val = (10 ** 10, -1)
     for lr in tested_lr_list:
+        logger.info(f'Testing learning rate {lr}')
         optimizer = optim(model.parameters(), lr=lr)
         results = train(model, train_loader, test_loader, optimizer, loss_fn, max_epochs, patience=patience,
                         save_best=False,
@@ -66,53 +102,72 @@ def find_best_start_lr(model: nn.Module, optim: Callable, train_loader: DataLoad
 
 def find_lr_steps(lr: float, model: nn.Module, optim: Callable,
                   train_loader: DataLoader, test_loader: DataLoader,
-                  loss_fn: nn.Module, patience: int = 3,
+                  loss_fn: nn.Module, patience: int = 5,global_patience:int=20,
                   device: Union[str, torch.device] = 'cuda', divisor: int = 10):
-    """
+    """Uses a given learning rate to determine the steps at which the learning rate should be decayed.
+    Uses trains the model using hte optimizer declared at optim with the data inside train_loader as training data and
+     uses the data inside val_loader as validation data. Calculates the losses with the loss_fn.
+     Trains the model on the device declared at device. Divides the learning rate by divisor at each step.
 
-    :param lr:
-    :param model:
-    :param optim:
-    :param train_loader:
-    :param test_loader:
-    :param loss_fn:
-    :param patience:
-    :param device:
-    :param divisor:
-    :return:
+    :param lr: Initial learning rate used
+    :param model: Model for which the learning rate schedule shall be determined
+    :param optim: Optimizer used for model training
+    :param train_loader: DataLoader containing the training data
+    :param test_loader: DataLoader containing the validation data
+    :param loss_fn: FUnctions used to calculate the losses
+    :param patience: A trial will get canceled if the validation loss did not improve in the last patience steps
+    :param device: Pytorch Device to train the model on
+    :param divisor: Number the learning rate will get divided by if it did not improve the val loss in the last patience steps
+    :return: Dictionary containing the learning rates and the number of last epoch in the trial where they reduced the val loss
     """
+    assert patience >= 1, AssertionError(
+        f'We need a patience of at least 1 for this method, because the model 1 training epoch after the best will get taken for the next lr decay trial')
     run_id = uuid.uuid1()
     reset_weight_path = f'D:/models/HMS/{run_id}.pth'
     torch.save(model.state_dict(), reset_weight_path)
     logger = get_logger(f'LR_TEST_{run_id}')
+    logger.info(f'Starting lr-step trial with ID: {run_id}')
     optimizer = optim(model.parameters(), lr=lr)
     best_val = 10 ** 10
     global_best_val = 10 ** 10
     cnt = 0
-    is_improving = True
     total_count = 0
+    global_patience_count = 0
     lr_step_dict = {}
+    #save_next = False
+    go_big = True
 
-    while is_improving:
-        is_improving = False
-        while 1:
-            train_step(model, train_loader, loss_fn, optimizer, device, False, logger)
+    while go_big:
+        go = True
+        while go:
+            train_val = train_step(model, train_loader, loss_fn, optimizer, device, False, logger)
             val = test_step(model, test_loader, loss_fn, device)
+            logger.info(
+                f"Learning Rate: {lr} | "
+                f"train_loss: {train_val:.4f} | "
+                f"val_loss: {val:.4f} | "
+            )
+            #if save_next:
+            #    torch.save(model.state_dict(), reset_weight_path)
+            #    save_next = False
             if val < best_val:
                 best_val = val
                 cnt = 0
                 if val < global_best_val:
                     global_best_val = val
                     torch.save(model.state_dict(), reset_weight_path)
-                    is_improving = True
+                    global_patience_count = 0
+                    #save_next = True
             if cnt == patience:
-                break
+                go = False
+            if global_patience_count == global_patience:
+                return lr_step_dict, global_best_val
             cnt += 1
             total_count += 1
+            global_patience_count += 1
         reset_model(model, reset_weight_path)
         lr_step_dict[lr] = total_count
         lr /= divisor
-    return lr_step_dict, global_best_val
 
 
 if __name__ == '__main__':
@@ -140,10 +195,10 @@ if __name__ == '__main__':
     model = SelfNet(**model_kwargs)
     logger = get_logger(name)
     df = pd.read_csv(Config.train_path)
-    df = prepare_train(df)
+    df = prepare_train(df).head(1000)
 
     # Nearly 80-20 split using patient ids
-    df_train, df_test = df.loc[df.patient_id>14000], df.loc[df.patient_id<=14000]
+    df_train, df_test = df.loc[df.patient_id > 14000], df.loc[df.patient_id <= 14000]
 
     specs = np.load(Config.full_data_path, allow_pickle=True).item()
 
@@ -162,9 +217,10 @@ if __name__ == '__main__':
 
     # Define Loss Function
     loss_fn = torch.nn.KLDivLoss(reduction='batchmean')
-    lr, results = find_best_start_lr(model=model, optim=optim, train_loader=train_loader, test_loader=test_loader,loss_fn=loss_fn)
-    logger.info(f'best starting LR found is {lr}/nAll results:{results}')
-    steps, best_loss = find_lr_steps(lr, model=model,optim=optim,train_loader=train_loader,test_loader=test_loader,loss_fn=loss_fn)
+    # lr, results = find_best_start_lr(model=model, optim=optim, train_loader=train_loader, test_loader=test_loader,
+    #                                 loss_fn=loss_fn)
+    # logger.info(f'best starting LR found is {lr}/nAll results:{results}')
+    steps, best_loss = find_lr_steps(0.001, model=model, optim=optim, train_loader=train_loader,
+                                     test_loader=test_loader,
+                                     loss_fn=loss_fn)
     logger.info(f'Reached best val_loss:{best_loss} with steps: {steps}')
-
-
